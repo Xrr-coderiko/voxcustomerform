ab921731face7c315772fdff6a7f14a0f73c4b0f
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-27 11:47:32 +0530

    w3fw

--- streamlit_app.py
+++ streamlit_app.py
@@ -1220,8 +1220,8 @@ with st.form(key="vendor_form", clear_on_submit=True):
             conn.update(worksheet="Vendors", data=updated_df)
 
             st.success("Details successfully submitted!")
-            
-with st.empty():
+with st.sidebar():            
+ with st.empty():
     st.write(Name)
     st.write(Phone)
     st.write(State)
