ec75046a40366d2cc56bf81413c1eb03339cab3c
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-28 00:19:05 +0530

    wqawd

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from streamlit_gsheets import GSheetsConnection
 import pandas as pd
 import re
-import plotly.express as px
+
 
 #st.image("./VOXlogo.jpeg",width=500,)
 cf1,cf2,cf3 = st.columns(3)
@@ -1351,20 +1351,11 @@ if 'SENT BY' in existing_data.columns:
     sentby_counts.columns = ['SENT BY', 'LEADS']
     st.dataframe(sentby_counts)
 if 'SOURCE' in existing_data.columns:
-        # Count the number of customers per state
+
         state_counts = existing_data['SOURCE'].value_counts().reset_index()
-        state_counts.columns = ['SOURCE', 'Number of Customers']
-
-        # Display the count of customers by state in a table
+        state_counts.columns = ['SOURCE', 'LEADS']
         st.dataframe(state_counts)
-
-        # Create a bar chart using Plotly
-        fig = px.bar(state_counts, x='SOURCE', y='Number of Customers', 
-                     title="Number of Customers by State", 
-                     labels={'Number of Customers':'Number of Customers', 'SOURCE':'SOURCE'})
-
-        # Display the bar chart in Streamlit
-        st.plotly_chart(fig)    
+        st.bar_chart(state_counts['LEADS'])
 
 
 st.sidebar.title(f"Total Lead: {total_rows}")          
