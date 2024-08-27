4d7590022f9d869b964fd0c000c88ac34c2e3d1e
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-27 12:07:29 +0530

    wef

--- streamlit_app.py
+++ streamlit_app.py
@@ -1222,7 +1222,6 @@ with st.form(key="vendor_form", clear_on_submit=True):
             st.success("Details successfully submitted!")
            
 with st.container(border=True):
-      st.text_area(Name,Phone,State)
       st.write(Name)
       st.write(Phone)
       st.write(State)
