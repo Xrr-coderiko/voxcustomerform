140f6f5db6fa951ed08068b2f8a9b89417f14aae
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-28 12:32:32 +0530

    wrtew

--- streamlit_app.py
+++ streamlit_app.py
@@ -1132,6 +1132,7 @@ PRODUCT = [
     "Wall",
     "Flooring",
     "Skirtings",
+    "Dealership",
 ]
 SOURCE = [
     " ",
@@ -1381,7 +1382,7 @@ with st.container(border=True):
   if 'SOURCE' in existing_data.columns:
         current_date_data = current_date_data.dropna(subset=['SOURCE'])
         source_count = current_date_data['SOURCE'].value_counts().reset_index()
-        source_count.columns = ['LEADS', 'SOURCE']
+        source_count.columns = ['SOURCE', 'LEADS']
         st.table(source_count.style.hide(axis="index"))
  st.bar_chart(source_count['SOURCE'])
 
