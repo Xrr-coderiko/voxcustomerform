a3534761160b2d9801febed54d6d6b30dd3a5a2b
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-08-28 15:42:25 +0530

    w3re

--- streamlit_app.py
+++ streamlit_app.py
@@ -1394,6 +1394,11 @@ with st.container(border=True):
         st.table(source_count.style.hide(axis="index"))
  
  st.bar_chart(source_count, x="SOURCE",y="LEADS")
+ if rdatarow in existing_data2:
+     rdata = rdata.dropna(subset=[rdatarow])
+     gen = rdata[rdatarow].value_counts().reset_index()
+     gen.columns=[rdatarow,"Data"]
+     st.table(gen)
 
 
 st.sidebar.title(f"Total Lead: {total_rows}")          
