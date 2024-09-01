d215b95639de7fcb1b898b994a7b5a155f359644
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-02 00:50:08 +0530

    sfdsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1482,8 +1482,8 @@ with tab3:
       st.table(xeta)
      with z2:
       st.markdown("<div style='text-align: center;'><h3>Distributor wise</h3></div>", unsafe_allow_html=True)
-      main_data=main_data.dropna(subset=['STENTO'])
-      peta = main_data['STENTO'].value_counts().reset_index()
+      main_data=main_data.dropna(subset=['STENTTO'])
+      peta = main_data['STENTTO'].value_counts().reset_index()
       peta.columns = ['STEN TO', 'LEADS']
       toc = beta['LEADS'].sum()
       toc = pd.DataFrame([['TOTAL', toc]], columns=['STEN TO', 'LEADS'])
