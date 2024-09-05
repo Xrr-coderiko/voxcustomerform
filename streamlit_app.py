b7b9796652b3fadc7646670b3f739bb8aedac5ff
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-05 13:40:29 +0530

    sfdsfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1538,7 +1538,8 @@ with tab4:
        st.write('please enter a city name to search for dealers.')
   with cy2:
     st.write("Add dealers")
-    if st.submit('Add Dealer'):
+    add_button = st.form_submit_button('Add Dealer')
+    if add_button: 
      DSTATUS = ['Active', 'Inactive']
      ddname = st.text_input(label="Dealer Name: ")
      ddcity = st.text_input(label="City: ")
