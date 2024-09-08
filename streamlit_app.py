92361b836d63db461953eddf314b47c28b6bfd93
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-08 23:29:22 +0530

    sfdsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1425,7 +1425,7 @@ with tab2:
      current_date_data = current_date_data.dropna(subset=['SENT BY'])
      sentby_counts = current_date_data['SENT BY'].value_counts().reset_index()
      sentby_counts.columns = ['CC Executives', 'QUALIFIED']
-     sentby_counts['ATTENDED'] = Atdata['ATTENDED'].value_counts().reindex(sentby_counts['SENT BY']).fillna(' ').values
+     sentby_counts['ATTENDED'] = Atdata['ATTENDED'].value_counts().reindex(sentby_counts['CC Executives']).fillna(' ').values
      totalcs = sentby_counts['QUALIFIED'].sum()
      totalca = sentby_counts['ATTENDED'].sum()
      total_row = pd.DataFrame([['TOTAL', totalcs, totalca]], columns=['CC Executives', 'ATTENDED', 'QUALIFIED'])
