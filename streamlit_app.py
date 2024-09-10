9587e48ee90d60ffec2ac71025f0225170b28a73
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-10 20:46:24 +0530

    sdfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1413,7 +1413,7 @@ with tab2:
   
  with st.container(border=True):
   #st.markdown(f"<div style='text-align: center;'><h2>{today2} QUALIFIED REPORT</h2></div>", unsafe_allow_html=True)
-  Attended = [10, 20, 10, 10, 10, 10]
+  Attended = [88, 65, 132, 48, 31, 119]
   
 
   #st.header(f"{today} QUALIFIED REPORT-------")    
