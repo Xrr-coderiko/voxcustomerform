fe4ab254b01940154db865ba1fc544e5e4e192c9
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-19 17:31:13 +0530

    sdfdsf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1392,7 +1392,7 @@ with tab1:
     if submit_button:
         # Check if all mandatory fields are filled
         is_valid = bool(pattern.match(Phone))
-        if not Name or not Phone or not State or not City or not District or not Sentto or not Product or not Source or not Sentby:
+        if not Name or not Phone or not State or not District or not Sentto or not Product or not Source or not Sentby:
             st.warning("Ensure all mandatory fields are filled.")
         elif not is_valid:
             st.warning("Incorrect Phone Number")
