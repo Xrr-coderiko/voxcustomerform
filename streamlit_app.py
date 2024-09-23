5028ad5964aba45d680b9570c2914a7686f4f885
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-23 21:24:58 +0530

    fsdfsdfd

--- streamlit_app.py
+++ streamlit_app.py
@@ -1516,6 +1516,7 @@ with tab2:
             vertical-align: middle !important;
           }
           .monthrep-div{
+             margin-top: -20px;
              thead th {display:none}
              tbody th {display:none}
           } 
@@ -1541,7 +1542,7 @@ with tab2:
      htmltbst = finldb.to_html(index=False)
      st.write(htmltbst, unsafe_allow_html=True) 
     with sex2:
-      st.markdown("<div style='text-align: center; border: 2px solid lightgrey; margin-bottom: -10px;'><h4>SEPTEMBER REPORT</h4></div>", unsafe_allow_html=True)
+      st.markdown("<div style='text-align: center; border: 2px solid lightgrey; margin-bottom: 5px;'><h4>SEPTEMBER REPORT</h4></div>", unsafe_allow_html=True)
       mrdf = pd.DataFrame(monthrep)
       st.markdown('<div class="monthrep-div"></div>',unsafe_allow_html=True)
       st.write(mrdf.to_html(index=False, header=False), unsafe_allow_html=True)
