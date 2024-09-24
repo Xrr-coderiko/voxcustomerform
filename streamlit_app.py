9dea1f32a83da05b7f3e41fa159433a7afa556ba
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-24 23:35:28 +0530

    fdsfdsfdf

--- streamlit_app.py
+++ streamlit_app.py
@@ -1480,7 +1480,7 @@ with tab2:
  with st.container(border=True):
    #st.markdown(f"<div style='text-align: center;'><h2>{today2} QUALIFIED REPORT</h2></div>", unsafe_allow_html=True)
   st.markdown(f"<div style='text-align: center; border: 2px solid Black;'><h2>{today2} LEADS REPORT</h2></div>", unsafe_allow_html=True)
-  Attended = [108, 51, 36, 45]
+  Attended = [68, 115, 32, 25, 17, 10]
   
 
   #st.header(f"{today} QUALIFIED REPORT-------")    
@@ -1528,12 +1528,12 @@ with tab2:
     if 'SENT BY' in existing_data.columns:
      current_date_data = current_date_data.dropna(subset=['SENT BY'])
      sentby_counts = current_date_data['SENT BY'].value_counts().reset_index()
-     #sentby_counts['ATTENDED'] = ]Attended
-     sentby_counts.columns = ['CC-EXECUTIVE', 'QUALIFIED']
+     sentby_counts['ATTENDED'] = Attended
+     sentby_counts.columns = ['CC-EXECUTIVE', 'QUALIFIED', 'ATTENDED']
      #.reindex(sentby_counts['CC Executives']).fillna(' ').values
      totalcs = sentby_counts['QUALIFIED'].sum()
-     #totalca = sentby_counts['ATTENDED'].sum()
-     total_row = pd.DataFrame([['TOTAL', totalcs]], columns=['CC-EXECUTIVE', 'QUALIFIED'])
+     totalca = sentby_counts['ATTENDED'].sum()
+     total_row = pd.DataFrame([['TOTAL', totalcs, totalca]], columns=['CC-EXECUTIVE', 'QUALIFIED', 'ATTENDED'])
      finldb = pd.concat([sentby_counts, total_row])
      htmltbst = finldb.to_html(index=False)
      st.write(htmltbst, unsafe_allow_html=True) 
