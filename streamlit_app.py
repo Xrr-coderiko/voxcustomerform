8ef276ce1a43cd31aff5ffe503905dae081720e6
Xrr-coderiko <vikaspatil5567@gmail.com>
2024-09-27 23:38:03 +0530

    DFVCXCX

--- streamlit_app.py
+++ streamlit_app.py
@@ -1495,7 +1495,7 @@ with tab2:
   cxxf1, cxxf2 = st.columns([2,1])            
   with cxxf1:  
         #st.markdown(f"<div style='text-align: center; border: 2px solid Black;'><h2>{today2} LEADS REPORT</h2></div>", unsafe_allow_html=True)
-        recdata[["RECEIVED", "ATTENDED",	"QUALIFIED"]] = recdata[["RECEIVED", "ATTENDED",	"QUALIFIED"]].fillna(0).astype(int)
+        recdata[["RECEIVED", "PENDING", "ATTENDED", "QUALIFIED"]] = recdata[["RECEIVED", "PENDING", "ATTENDED",	"QUALIFIED"]].fillna(0).astype(int)
         recdata_reset = recdata.reset_index(drop=True)
         hide_table_row_index = """
         <style> table {
@@ -1536,7 +1536,7 @@ with tab2:
     if 'SENT BY' in existing_data.columns:
      current_date_data = current_date_data.dropna(subset=['SENT BY'])
      sentby_counts = current_date_data['SENT BY'].value_counts().reset_index()
-     sentby_counts['ATTENDED'] = current_date_data['SENT BY'].count()
+     sentby_counts['ATTENDED'] = Attended
      sentby_counts.columns = ['CC-EXECUTIVE', 'QUALIFIED', 'ATTENDED']
      #.reindex(sentby_counts['CC Executives']).fillna(' ').valsdfsfues
      totalcs = sentby_counts['QUALIFIED'].sum()
